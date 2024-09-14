bacb629f89b4df04bd22aee4fbddec9f936005fd
RC-6989 <75128887+RC-6989@users.noreply.github.com>
2024-09-14 08:56:51 -0400

    Update Speech_Detection.py

---
+++ MachineLearningPredictions/Speech_Detection.py
@@ -0,0 +1,22 @@
+import speech_recognition as sr
+
+
+r = sr.Recognizer() 
+
+
+
+def recognize_wav(wav_file):    
+    try:
+        with sr.AudioFile(wav_file) as source2:
+            print("Listening")
+            audio2 = r.listen(source2)
+            MyText = r.recognize_google(audio2)
+            MyText = MyText.lower()
+
+            print(MyText)
+            
+    except sr.RequestError as e:
+        print("Unable to recognize audio. ")
+        
+    except sr.UnknownValueError:
+        print("Unknown file value. File not found/File Corrupted. ")
