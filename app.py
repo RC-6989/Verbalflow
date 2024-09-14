42d7bf14e055a05274055ab418228a1f8ff5567f
rdnm0 <rohandhanam@gmail.com>
2024-09-14 08:52:21 -0400

    css changes

--- app.py
+++ app.py
@@ -1,22 +1,17 @@
 import os
 from flask import Flask, request, redirect, url_for, flash, render_template, send_from_directory
 from werkzeug.utils import secure_filename
-import whisper
-from MachineLearningPredictions.Emotion_Detection import detect_emotion
 
+# from MachineLearningPredictions.Emotion_Detection import detect_emotion
 
 # Starts the app
 app = Flask(__name__)
 
-def talking(audioPath):
-    model = whisper.load_model("base")
-    result = model.transcribe(audioPath)
-    return result["text"]
 
 
 @app.route("/")
 def home():
-    return render_template('index.html', var1=emotion)
+    return render_template('index.html')
 
 # Basically app.use() in express
 @app.route('/public/<path:path>')
@@ -30,7 +25,7 @@ def upload_frame():
         return "No frame part in the request", 400
     file = request.files['frame']
     if file:
-        emotion = detect_emotion(file)
+        # emotion = detect_emotion(file)
         return "Frame received and saved", 200
     return "Failed to upload frame", 400
 
