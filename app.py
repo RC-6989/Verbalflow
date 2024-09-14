f977c4720f9aa9954fc2e60c342d9a29b96c9faf
Rohit Chavali <rohitchavali2008@gmail.com>
2024-09-14 04:58:08 -0400

    I deleted all the files L

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import os
 from flask import Flask, request, redirect, url_for, flash, render_template, send_from_directory
 from werkzeug.utils import secure_filename
 import whisper
+from MachineLearningPredictions.Emotion_Detection import detect_emotion
+
+emotion = None
+print("Renew?")
 
 # Starts the app
 app = Flask(__name__)
@@ -14,25 +18,12 @@ def talking(audioPath):
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html', var1=emotion)
 
 # Basically app.use() in express
 @app.route('/public/<path:path>')
 def send_report(path):
     return send_from_directory('public', path)
-
-
-# Route to handle image upload
-@app.route('/upload', methods=['POST'])
-def upload_image():
-    # Secure the filename and save the file to the desired folder
-    filename = secure_filename(file.filename)
-    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    file.save(file_path)
-    return f"File uploaded successfully: {filename}"
-
-UPLOAD_FOLDER = 'frames/'
-app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(os.path.abspath(__file__)), UPLOAD_FOLDER)
 
 # Get video
 @app.route('/upload_frame', methods=['POST'])
@@ -41,13 +32,13 @@ def upload_frame():
         return "No frame part in the request", 400
     file = request.files['frame']
     if file:
-        filename = secure_filename(file.filename)
-        print("The file name is " + filename)
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(file_path)
+        emotion = detect_emotion(file)
         return "Frame received and saved", 200
     return "Failed to upload frame", 400
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
 
