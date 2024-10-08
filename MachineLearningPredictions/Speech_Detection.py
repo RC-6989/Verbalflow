72dacfe01a849593f70419510dd75e88994a2318
Rohit Chavali <rohitchavali2008@gmail.com>
2024-10-08 08:47:09 -0400

    removed exception handling in transcript detection

--- MachineLearningPredictions/Speech_Detection.py
+++ MachineLearningPredictions/Speech_Detection.py
@@ -3,7 +3,6 @@ import speech_recognition as sr
 r = sr.Recognizer() 
 
 def detect_wav(wav_file):    
-    try:
         with sr.AudioFile(wav_file) as source2:
             print("Listening")
             audio2 = r.listen(source2)
@@ -11,11 +10,7 @@ def detect_wav(wav_file):
             MyText = MyText.lower()
 
             return MyText
-    except sr.RequestError as e:
-        print("Unable to recognize audio. ")
         
-    except sr.UnknownValueError:
-        print("Unknown file value. File not found/File Corrupted. ")
 
 if __name__ == "__main__":
     print(detect_wav("/Users/rohitchavali/Desktop/Verbalflow/recordedFiles/audio.wav"))
